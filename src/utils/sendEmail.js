a3c51e76ddcca3708d06b9e67b352fc2acbcedc0
jayant <iamvermajayant@gmail.com>
2025-10-18 12:48:12 +0530

    changing the smtp server from gmail to resend

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,12 +1,33 @@
-import transporter from "../config/email.js";
+// import transporter from "../config/email.js";
 
-export const sendEmail = async ({ to, subject, html }) => {
-  const mailOptions = {
+// export const sendEmail = async ({ to, subject, html }) => {
+//   const mailOptions = {
+//     from: process.env.EMAIL_FROM,
+//     to,
+//     subject,
+//     html,
+//   };
+
+//   return transporter.sendMail(mailOptions);
+// };
+
+
+import { Resend } from "resend";
+
+const resend = new Resend('re_V9FDLN6Z_CFekYZTdvgc1dDRW5TtpRwwA');
+
+export const sendEmail = async ({ to, subject, html, text }) => {
+  const { data, error } = await resend.emails.send({
     from: process.env.EMAIL_FROM,
-    to,
+    to: Array.isArray(to) ? to : [to],
     subject,
     html,
-  };
+    text,
+  });
 
-  return transporter.sendMail(mailOptions);
+  if (error) {
+    console.error("Resend email error:", error);
+    throw new Error("Failed to send email");
+  }
+  return data;
 };
